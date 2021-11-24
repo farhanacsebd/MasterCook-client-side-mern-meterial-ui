770056ca8a27e597059904880026bf20aa888ddd
faisalcsebd <farhana.cse.bd@gmail.com>
2021-11-24 23:52:27 +0600

    navigation

---
+++ src/Pages/NotFound/NotFound.js
@@ -0,0 +1,10 @@
+import React from 'react';
+const Notfound = () => {
+    return (
+        <div>
+            <img style={{ width: "100%", height: "400px" }} src="https://www.pngkit.com/png/detail/930-9306658_404-not-found.png" alt="" />
+        </div>
+    );
+};
+
+export default Notfound;
